2be00c7d76f03f6624ea9e6b8191a48b13beaec1
Xerminator13 <haydenfcorbin@gmail.com>
2021-03-19 21:24:55 -0400

    Full functionality for the Add To Field button

--- app/src/main/java/com/example/macows/ScoreScreen.java
+++ app/src/main/java/com/example/macows/ScoreScreen.java
@@ -11,7 +11,7 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ScoreScreen extends AppCompatActivity {
-    private Button addToField, addToBarn, roadKill, resurrectZombieCows, church, police, cemetery, fastFood, stockTrailer, funeralHome, hospital, school, back;
+    private Button addToField, addToBarn, takeFromBarn, roadKill, resurrectZombieCows, church, police, cemetery, fastFood, stockTrailer, funeralHome, hospital, school, back;
     private EditText scoreEntry;
     private TextView playerNameLabel, numCowsInField, numCowsInBarn, numZombieCows, herdManagement, animalHusbandry, errorLabel;
 
@@ -26,10 +26,33 @@ public class ScoreScreen extends AppCompatActivity {
     private void initAllElements() {
         playerNameLabel = findViewById(R.id.playerNameLabel);
         scoreEntry = findViewById(R.id.scoreEntry);
+        addToField = findViewById(R.id.addToField);
+        addToBarn = findViewById(R.id.addToBarn);
+        takeFromBarn = findViewById(R.id.takeFromBarn);
+
+        errorLabel = findViewById(R.id.errorLabel);
         back = findViewById(R.id.scoreScreenToMainMenu);
 
         //Temporary arrangement
-        numCowsInField = findViewById(R.id.textView4);
+        numCowsInField = findViewById(R.id.errorLabel);
+
+    }
+    private boolean isInteger(String a) {
+        if (a != null) {
+            try {
+                Integer.parseInt(a);
+                return true;
+
+            } catch (Exception e) {
+                return false;
+
+            }
+
+        }
+        else {
+            return false;
+
+        }
 
     }
 
@@ -48,6 +71,33 @@ public class ScoreScreen extends AppCompatActivity {
 
         //------------------------------------------------------------------------------------------
 
+        addToField.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String input1 = scoreEntry.getText().toString();
+                if (isInteger(input1)) {
+                    int input2 = Integer.parseInt(input1);
+
+                    if (input2 > 0) {
+                        CommonUtils.playerList.get(CommonUtils.getCurrentPlayer() - 1).addCowsToField(input2);
+                        CommonUtils.updatePlayerPrefs(CommonUtils.playerList.get(CommonUtils.getCurrentPlayer() - 1), CommonUtils.getCurrentPlayer() - 1);
+                        errorLabel.setText(CommonUtils.formatPlayerPrefs(CommonUtils.getCurrentPlayer()));
+
+                    }
+                    else {
+                        errorLabel.setText("You cannot input a negative number of cows");
+
+                    }
+
+                }
+                else {
+                    errorLabel.setText("You did not input a whole number of cows, please try again.");
+
+                }
+
+
+            }
+
+        });
         back.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 startActivity(new Intent(ScoreScreen.this, MainMenu.class));
