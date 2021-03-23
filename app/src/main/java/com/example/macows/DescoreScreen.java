83221a01676689a3898b4ed2477b5d237123a6cc
Xerminator13 <haydenfcorbin@gmail.com>
2021-03-23 15:31:54 -0400

    I have implemented near full descoring.  the logic is not correct and TextViews are not updating as they should, but the app is not crashing due to an IndexOutOfBounds error somewhere in the code, so I think I have made a ton of progress.

--- app/src/main/java/com/example/macows/DescoreScreen.java
+++ app/src/main/java/com/example/macows/DescoreScreen.java
@@ -9,10 +9,16 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class DescoreScreen extends AppCompatActivity {
-    private Button player1, player2, player3, player4, player5, player6;
+    private ArrayList<TextView> textViewList = new ArrayList<TextView>();
+    private ArrayList<Button> buttonList = new ArrayList<Button>();
+    int[] players = new int[5]; //holds as indices in CommonUtils.playerList ArrayList
+
+    private Button player1, player2, player3, player4, player5;
     private Button back, descore;
-    private TextView p1Field, p1Barn, p2Field, p2Barn, p3Field, p3Barn, p4Field, p4Barn, p5Field, p5Barn, p6Field, p6Barn;
+    private TextView p1Field, p1Barn, p2Field, p2Barn, p3Field, p3Barn, p4Field, p4Barn, p5Field, p5Barn;
     private TextView label, errorLabeling;
 
     int currentPlayer = CommonUtils.getCurrentPlayer(); //This returns the index of the player + 1
@@ -28,17 +34,84 @@ public class DescoreScreen extends AppCompatActivity {
         //Buttons
         descore = findViewById(R.id.confirmationButton);
         back = findViewById(R.id.cancelButton);
+        player1 = findViewById(R.id.player1ConfirmButton);
+        player2 = findViewById(R.id.player2ConfirmButton);
+        player3 = findViewById(R.id.player3ConfirmButton);
+        player4 = findViewById(R.id.player4ConfirmButton);
+        player5 = findViewById(R.id.player5ConfirmButton);
+
+        buttonList.add(player1);
+        buttonList.add(player2);
+        buttonList.add(player3);
+        buttonList.add(player4);
+        buttonList.add(player5);
 
         //TextViews
         errorLabeling = findViewById(R.id.errorLabeling);
+        p1Field = findViewById(R.id.player1FieldView);
+        p1Barn = findViewById(R.id.player1BarnView);
+        p2Field = findViewById(R.id.player2FieldView);
+        p2Barn = findViewById(R.id.player2BarnView);
+        p3Field = findViewById(R.id.player3FieldView);
+        p3Barn = findViewById(R.id.player3BarnView);
+        p4Field = findViewById(R.id.player4FieldView);
+        p4Barn = findViewById(R.id.player4BarnView);
+        p5Field = findViewById(R.id.player5FieldView);
+        p5Barn = findViewById(R.id.player5BarnView);
+
+        textViewList.add(p1Field);
+        textViewList.add(p1Barn);
+        textViewList.add(p2Field);
+        textViewList.add(p2Barn);
+        textViewList.add(p3Field);
+        textViewList.add(p3Barn);
+        textViewList.add(p4Field);
+        textViewList.add(p4Barn);
+        textViewList.add(p5Field);
+        textViewList.add(p5Barn);
+
 
     }
     private void populateTextViews() {
         //Populate text views here
         errorLabeling.setText("No player has been selected yet!!");
 
+        //Buttons;  Extra code for naming the buttons with player names
+        int b = 0;
+        for (int a = 0; a < 6; a++) {
+            if (a != currentPlayer - 1) {
+                players[b] = a;
+                b++;
+
+            }
+            else {
+                Log.d("PopulateList", "Skipped index" + b);
+
+            }
+
+        }
+
+        for (int a : players) {
+            System.out.println(a);
+
+        }
+
+        player1.setText(CommonUtils.playerList.get(players[0]).getName());
+        player2.setText(CommonUtils.playerList.get(players[1]).getName());
+        player3.setText(CommonUtils.playerList.get(players[2]).getName());
+        player4.setText(CommonUtils.playerList.get(players[3]).getName());
+        player5.setText(CommonUtils.playerList.get(players[4]).getName());
+
+        b = 0;
+        for (int a = 0; a < textViewList.size(); a += 2) {
+            textViewList.get(a).setText("Cows in field:  " + CommonUtils.playerList.get(b).getCowsInField());
+            textViewList.get(a + 1).setText("Cows in barn:  " + CommonUtils.playerList.get(b).getCowsInBarn());
+            b++;
+
+        }
+
     }
-    private boolean descorePlayer(int player) {
+    private boolean descorePlayer(int player) { //player == playerTakenFrom > represents index of player + 1
         int a = player - 1;
 
         if (player == -1) {
@@ -47,7 +120,41 @@ public class DescoreScreen extends AppCompatActivity {
 
         }
         else {
-            //Implement descoring methods here
+            if (descoreMethod == 1) {
+                //Cemetery
+                CommonUtils.playerList.get(currentPlayer - 1).sawCemetery(CommonUtils.playerList.get(a));
+                CommonUtils.updatePlayerPrefs(CommonUtils.playerList.get(currentPlayer - 1), currentPlayer - 1);
+                CommonUtils.updatePlayerPrefs(CommonUtils.playerList.get(a), a);
+
+            }
+            else if (descoreMethod == 2) {
+                //Fast Food
+                CommonUtils.playerList.get(currentPlayer - 1).sawFastFood(CommonUtils.playerList.get(a));
+                CommonUtils.updatePlayerPrefs(CommonUtils.playerList.get(currentPlayer - 1), currentPlayer - 1);
+                CommonUtils.updatePlayerPrefs(CommonUtils.playerList.get(a), a);
+
+            }
+            else if (descoreMethod == 3) {
+                //Police
+                CommonUtils.playerList.get(currentPlayer - 1).sawPolice(CommonUtils.playerList.get(a));
+                CommonUtils.updatePlayerPrefs(CommonUtils.playerList.get(currentPlayer - 1), currentPlayer - 1);
+                CommonUtils.updatePlayerPrefs(CommonUtils.playerList.get(a), a);
+
+            }
+            else if (descoreMethod == 4) {
+                //Stock Trailer
+                CommonUtils.playerList.get(currentPlayer - 1).sawStockTrailer(CommonUtils.playerList.get(a));
+                CommonUtils.updatePlayerPrefs(CommonUtils.playerList.get(currentPlayer - 1), currentPlayer - 1);
+                CommonUtils.updatePlayerPrefs(CommonUtils.playerList.get(a), a);
+
+            }
+            else if (descoreMethod == 5) {
+                //funeral Home
+                CommonUtils.playerList.get(currentPlayer - 1).sawFuneralHome(CommonUtils.playerList.get(a));
+                CommonUtils.updatePlayerPrefs(CommonUtils.playerList.get(currentPlayer - 1), currentPlayer - 1);
+                CommonUtils.updatePlayerPrefs(CommonUtils.playerList.get(a), a);
+
+            }
 
         }
         return true;
@@ -66,6 +173,20 @@ public class DescoreScreen extends AppCompatActivity {
 
         initAllElements();
         populateTextViews();
+
+        for (int a = 0; a < buttonList.size(); a++) {
+            int zA = a;
+            buttonList.get(a).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    playerTakenFrom = players[zA] + 1;
+                    errorLabeling.setText("You are taking cows from " + CommonUtils.playerList.get(players[zA]).getName());
+
+                }
+
+            });
+
+        }
 
         //------------------------------------------------------------------------------------------
 
